bd7fadfdbea8dc7fb2b7cd350e7cffeb0b6b669d
marvin <marvin@thismarvin.com>
2021-04-09 23:24:51 -0500

    feature: setup useful enums

---
+++ src/utils.cpp
@@ -0,0 +1,15 @@
+namespace utils {
+    enum ChessPiece {
+        None = 0,
+        Pawn = 1,
+        Bishop = 2,
+        Knight = 4,
+        Rook = 8,
+        Queen = 16,
+        King = 32,
+    };
+    enum Player {
+        White = 0,
+        Black = 1,
+    };
+}
