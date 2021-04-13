34015f56e5064e403e0076cc9c63754749ad169c
marvin <marvin@thismarvin.com>
2021-04-12 22:32:25 -0500

    feature: reworked api foundation

--- src/dam.hpp
+++ src/dam.hpp
@@ -5,6 +5,14 @@
 #include <allegro5/color.h>
 
 namespace dam {
-    void draw_text(const char* text, const ALLEGRO_FONT* font, float x, float y, float size);
+	template <typename T>
+	void draw_text(const char* text, T* font, float x, float y, float size);
+	template <typename T>
+	void draw_rect(float x, float y, float width, float height, T color);
+	template <typename T>
+	void draw_quad(float x1, float y1, float x2, float y2, T color);
+	template <typename T>
+	void draw_texture(float x, float y, T texture);
 }
+#include "dam_allegro.hpp"
 #endif
---
+++ src/dam_allegro.hpp
@@ -0,0 +1,22 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
+
+namespace dam {
+	template <typename T>
+	void draw_text(const char* text, T* font, float x, float y, float size) {
+		al_draw_text(font, al_map_rgb_f(255, 255, 255), x, y, 0, text);
+	}
+	template <typename T>
+	void draw_rect(float x, float y, float width, float height, T color) {
+		al_draw_filled_rectangle(x, y, x + width, y + height, color);
+	}
+	template <typename T>
+	void draw_quad(float x1, float y1, float x2, float y2, T color) {
+		al_draw_filled_rectangle(x1, y1, x2, y2, color);
+	}
+	template <typename T>
+	void draw_texture(float x, float y, T texture) {
+		al_draw_bitmap(texture, x, y, 0);
+	}
+}
