ef64a500fc83d729b5510f6d3b59140e699705ab
marvin <marvin@thismarvin.com>
2021-04-18 23:04:44 -0500

    feature: added Color class

---
+++ src/color.cpp
@@ -0,0 +1,19 @@
+#include "color.hpp"
+
+dam::Color::Color(unsigned int hexadecimal, float alpha)
+{
+    r = ((hexadecimal & 0xff0000) >> 16) / 255;
+    g = ((hexadecimal & 0xff00) >> 8) / 255;
+    b = (hexadecimal & 0xff) / 255;
+    a = 1;
+
+    multiply(alpha);
+}
+
+void dam::Color::multiply(float value)
+{
+    r = r * value;
+    g = g * value;
+    b = b * value;
+    a = a * value;
+}
---
+++ src/color.hpp
@@ -0,0 +1,16 @@
+#ifndef COLOR_HPP
+#define COLOR_HPP
+namespace dam {
+class Color;
+}
+
+class dam::Color {
+public:
+    Color(unsigned int hexadecimal, float alpha=1);
+    float r;
+    float g;
+    float b;
+    float a;
+    void multiply(float value);
+};
+#endif
