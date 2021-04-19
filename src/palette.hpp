151862466de2af0aabe2fa064da546121d19b23c
marvin <marvin@thismarvin.com>
2021-04-18 23:06:11 -0500

    feature: added a palette namespace

---
+++ src/palette.hpp
@@ -0,0 +1,14 @@
+#ifndef PALETTE_HPP
+#define PALETTE_HPP
+#include "color.hpp"
+
+namespace dam {
+namespace palette {
+const dam::Color WHITE = dam::Color(0xffffff);
+const dam::Color BLACK = dam::Color(0x000000);
+const dam::Color RED = dam::Color(0xff0000);
+const dam::Color GREEN = dam::Color(0x00ff00);
+const dam::Color BLUE = dam::Color(0x0000ff);
+}
+}
+#endif
