a0ae27b967cd38e25e5f2e0e979b4b6a538a875d
marvin <marvin@thismarvin.com>
2021-05-17 14:21:05 -0500

    chore: forgot to format code

--- src/dam/image_region.cpp
+++ src/dam/image_region.cpp
@@ -1,13 +1,15 @@
 #include "image_region.hpp"
 
-dam::graphics::ImageRegion::ImageRegion() {
+dam::graphics::ImageRegion::ImageRegion()
+{
     this->x = 0;
     this->y = 0;
     this->width = 0;
     this->height = 0;
 }
 
-dam::graphics::ImageRegion::ImageRegion(unsigned int x, unsigned int y, unsigned int width, unsigned int height) {
+dam::graphics::ImageRegion::ImageRegion(unsigned int x, unsigned int y, unsigned int width, unsigned int height)
+{
     this->x = x;
     this->y = y;
     this->width = width;
