792cbbb1a2980673bff0da7eba466bdcde6cdc43
marvin <marvin@thismarvin.com>
2021-06-10 13:50:08 -0500

    feature: added CastlingRights enum

--- src/chess/utils.hpp
+++ src/chess/utils.hpp
@@ -20,3 +20,11 @@ enum class Team {
     White = 1,
     Black = 2,
 };
+
+enum class CastlingRights {
+    None = 0,
+    WhiteKingSide = 1,
+    WhiteQueenSide = 2,
+    BlackKingSide = 4,
+    BlackQueenSide = 8,
+};
