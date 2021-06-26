01ac889467a92a830deb7cc47f67002bf97b45bf
marvin <marvin@thismarvin.com>
2021-06-26 10:13:44 -0500

    feature: RectangleF const-correctness/encapsulation pass

--- src/dam/rectanglef.hpp
+++ src/dam/rectanglef.hpp
@@ -7,11 +7,34 @@ class RectangleF;
 class dam::RectangleF {
 public:
     RectangleF();
-    RectangleF(float x, float y, float width, float height);
+    RectangleF(float x, float y, float width, float height) :
+        m_x(x),
+        m_y(y),
+        m_width(width),
+        m_height(height)
+    {
+    }
 
-    float x;
-    float y;
-    float width;
-    float height;
+    float x() const
+    {
+        return m_x;
+    }
+    float y() const
+    {
+        return m_y;
+    }
+    float width() const
+    {
+        return m_width;
+    }
+    float height() const
+    {
+        return m_height;
+    }
+private:
+    float m_x { 0 };
+    float m_y { 0 };
+    float m_width { 0 };
+    float m_height { 0 };
 };
 #endif
