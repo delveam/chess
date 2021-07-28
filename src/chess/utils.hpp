9ff6b87ca71a3571000bd716bd7f0ba8ee5a3cf1
marvin <marvin@thismarvin.com>
2021-07-27 22:34:29 -0500

    chore: moved constants namespace to its own file

--- src/chess/utils.hpp
+++ src/chess/utils.hpp
@@ -3,14 +3,6 @@
 #include <string>
 #include <vector>
 
-namespace constants {
-constexpr int board_width = 8;
-constexpr int board_height = 8;
-constexpr int board_size = board_width * board_height;
-const std::string fen_regex = "((?:[pbnrqkPBNRQK1-8]+\\/){7}[pbnrqkPBNRQK1-8]+)\\s+([wb])\\s+((?!\\s)K?Q?k?q?|-)\\s+((?:[a-h][36])|-)\\s+(\\d+)\\s+(\\d+)";
-const std::string starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-}
-
 namespace utils {
 std::vector<std::string> split(std::string string, std::string delimiter);
 std::vector<std::string> split_whitespace(std::string string);
