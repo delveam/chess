b23e87bab8a24dd0c73e9e20593e0d265d53fd26
marvin <marvin@thismarvin.com>
2021-07-27 22:10:46 -0500

    feature: added fen validation regex to constants

--- src/chess/utils.hpp
+++ src/chess/utils.hpp
@@ -7,6 +7,7 @@ namespace constants {
 constexpr int board_width = 8;
 constexpr int board_height = 8;
 constexpr int board_size = board_width * board_height;
+const std::string fen_regex = "((?:[pbnrqkPBNRQK1-8]+\\/){7}[pbnrqkPBNRQK1-8]+)\\s+([wb])\\s+((?!\\s)K?Q?k?q?|-)\\s+((?:[a-h][36])|-)\\s+(\\d+)\\s+(\\d+)";
 const std::string starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 }
 
