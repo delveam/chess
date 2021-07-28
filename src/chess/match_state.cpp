1946c62033f196045a38f82486c531c456b0e519
marvin <marvin@thismarvin.com>
2021-07-28 12:56:50 -0500

    feature: made use of gm::Analysis's contains_move method

--- src/chess/match_state.cpp
+++ src/chess/match_state.cpp
@@ -2,16 +2,7 @@
 
 std::optional<MatchState> MatchState::submit_move(Move move) const
 {
-    auto start = move.start();
-    auto start_index = start.y() * constants::board_width + start.x();
-
-    // Note that this if statement acts as a guard.
-    if (m_analysis.moves().count(start_index) == 0) {
-        return std::nullopt;
-    }
-
-    // Because of the guard above, this if statement (which is also a guard) shouldn't throw an exception.
-    if (m_analysis.moves().at(start_index).count(move.lan()) == 0) {
+    if (!m_analysis.contains_move(move.lan())) {
         return std::nullopt;
     }
 
